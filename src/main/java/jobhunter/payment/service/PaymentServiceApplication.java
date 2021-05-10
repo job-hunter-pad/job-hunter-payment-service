99fb155960157d1ad57c7e947d82dd9cb0e055da
Andrei Timar <atimar999@gmail.com>
2021-05-10 18:48:00 +0300

    Use Environment Variables

--- src/main/java/jobhunter/payment/service/PaymentServiceApplication.java
+++ src/main/java/jobhunter/payment/service/PaymentServiceApplication.java
@@ -1,13 +1,18 @@
 package jobhunter.payment.service;
 
+import com.stripe.Stripe;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaymentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        Stripe.apiKey = dotenv.get("STRIPE_SECRET");
+
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
 
 }
